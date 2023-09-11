1817742db690dfd2ee74b4a3010e27a76da7c9fe
muhammadashir123 <127128121+muhammadashir123@users.noreply.github.com>
2023-09-11 13:28:41 -0400

    Update page.jsx

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import "@uploadthing/react/styles.css";
 import { UploadButton } from "@/src/utils/uploadthing";
-import Image from " next/image";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 import React, { useState, useEffect } from "react";
